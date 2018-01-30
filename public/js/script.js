1f486920345495f3e244f930190dee3f53845104
Ben <haas.ben4@gmail.com>
2018-01-30 13:37:47 -0700

    Add toggleDrum function

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,11 @@ let kicks = [false, false, false, false, false, false, false, false, false, fals
 let snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+const toggleDrum = (instrument, index) => {
+  if (instrument[index]) {
+    instrument[index] = false;
+  } else if (!instrument[index]) {
+    instrument[index] = true;
+  }
+};
