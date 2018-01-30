02ff0778be59dd9ed0c62e616c412b45ce0b49c4
Ben <haas.ben4@gmail.com>
2018-01-30 13:49:21 -0700

    Add test logs

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,18 @@ let hiHats = [false, false, false, false, false, false, false, false, false, fal
 let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
 const toggleDrum = (drum, index) => {
-  drum[inded] ? false : true;
+  drum[index] ? false : true;
+  console.log('toggle');
 };
 
 const clear = drums => {
   drums.forEach(drum => drum = false);
+  console.log('clear');
 };
 
 const invert = drums => {
   drums.forEach(function(drum) {
     drum ? false : true;
+    console.log('invert');
   });
 }
