2782cf14537c46ae66f8c1bfd71d84a5542338ab
Ben <haas.ben4@gmail.com>
2018-01-30 13:46:41 -0700

    Add invert function

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,15 @@ let hiHats = [false, false, false, false, false, false, false, false, false, fal
 let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
 const toggleDrum = (drum, index) => {
-  if (drum[index]) {
-    drum[index] = false;
-  } else if (!drum[index]) {
-    drum[index] = true;
-  }
+  drum[inded] ? false : true;
 };
 
 const clear = drums => {
-  drums.forEach(function(drum) {
-    drum = false;
-  });
+  drums.forEach(drum => drum = false);
 };
+
+const invert = drums => {
+  drums.forEach(function(drum) {
+    drum ? false : true;
+  });
+}
