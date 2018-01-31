b16950bf51d5d59b4d439500372cb4e104d99831
Ben Haas <haas.ben4@gmail.com>
2018-01-30 21:21:56 -0700

    Finish main functions

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,37 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const getPreset = (index) => {
+  return presets[index] || null;
+}
+
+const putPreset = (index, arrayName) => {
+  if (!presets[index]) {
+    return;
+  }
+  presets[index] = arrayName;
+  return presets[index];
+};
+
+const presetHandler = (method, index, newPresetArray) => {
+  if (method === 'GET') {
+    let preset = getPreset(index);
+    if (preset) {
+      return [200, preset];
+    } else {
+      return [404];
+    }
+  } else if (method === 'PUT') {
+    const newPreset = putPreset(index, newPresetArray);
+    if (newPreset) {
+      return [200, newPreset];
+    } else {
+      return [404];
+    }
+  } else {
+    return [400];
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -4,19 +4,64 @@ let snares = [false, false, false, false, false, false, false, false, false, fal
 let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
-const toggleDrum = (drum, index) => {
-  drum[index] ? false : true;
-  console.log('toggle');
+const getArray = (arrayName) => {
+  switch (arrayName) {
+    case 'kicks':
+      return kicks;
+      break;
+    case 'snares':
+      return snares;
+      break;
+    case 'hiHats':
+      return hiHats;
+      break;
+    case 'rideCymbals':
+      return rideCymbals;
+      break;
+    default:
+      return false;
+  }
 };
 
-const clear = drums => {
-  drums.forEach(drum => drum = false);
-  console.log('clear');
+const toggleDrum = (drumArray, index) => {
+  let drumSet = getArray(drumArray);
+
+  if (!drumArray || index > 15 || index < 0) {
+    return;
+  }
+
+  drumSet[index] = !drumSet[index];
+
 };
 
-const invert = drums => {
-  drums.forEach(function(drum) {
-    drum ? false : true;
-    console.log('invert');
-  });
-}
+const clear = drumArray => {
+  let drumSet = getArray(drumArray);
+  if (!drumArray || !drumSet) {
+    return;
+  }
+
+  drumSet.fill(false);
+};
+
+const invert = drumArray => {
+  let drumSet = getArray(drumArray);
+  if (!drumArray || !drumSet) {
+    return;
+  }
+
+  for (let i=0; i<drumSet.length; i++) {
+    drumSet[i] = !drumSet[i];
+  }
+};
+
+const getNeighborPads = (x, y, size) => {
+  let neighborArray = [];
+  if (x > size-1 || x < 0 || y > size-1 || y < 0) {
+    return [];
+  } else {
+    //TODO add neighbor logic
+    // neighborArray.push([x, y+1], [x,y-1], [x+1,y], [x-1,y]);
+  }
+
+  return neighborArray;
+};
